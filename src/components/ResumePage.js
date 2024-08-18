b8d8b17d7af4ecf92e941e9076e4ea2ff4afa8a5
Shalini Singh <48309217+usrminie@users.noreply.github.com>
2024-08-18 20:57:05 +0530

    Update ResumePage.js

--- src/components/ResumePage.js
+++ src/components/ResumePage.js
@@ -1,17 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
 import ThemeToggle from './ThemeToggle';
-import Navbar from './Navbar';
+import logo from '../assets/logo.png';
 import Footer from './Footer';
 
 const ResumePage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="min-h-screen bg-gradient-to-r from-gray-100 to-gray-300 dark:from-gray-800 dark:to-gray-90 text-gray-900 dark:text-white flex flex-col justify-between">
-      <Navbar />
-      <div className="relative flex-grow container mx-auto px-4 py-8 flex flex-col items-center">
-        <div className="absolute top-4 right-4 z-50">
+    <div className="min-h-screen bg-gradient-to-r from-gray-100 to-gray-300 dark:from-gray-800 dark:to-gray-900 text-gray-900 dark:text-white flex flex-col justify-between">
+      <div className="absolute top-4 left-4 flex items-center space-x-4">
+        <img src={logo} alt="Logo" className="h-10 w-10 rounded-full shadow-md border-2 border-white ml-4" />
+        <div className="text-xl font-extrabold">Shalini Singh</div>
+      </div>
+      {/* Menu items centered at the top */}
+      <div className="absolute top-4 inset-x-0 flex justify-center items-center">
+        <div className="hidden md:flex md:items-center space-x-4 bg-white bg-opacity-20 dark:bg-gray-700 dark:bg-opacity-50 rounded-full px-6 py-2">
+          <a href="/" className="block px-4 py-2 text-gray-900 dark:text-white hover:bg-opacity-70 hover:bg-blue-900 dark:hover:bg-blue-700 transition-all rounded-md font-bold">
+            Home
+          </a>
+          <a href="#education" className="block px-4 py-2 text-gray-900 dark:text-white hover:bg-opacity-70 hover:bg-blue-900 dark:hover:bg-blue-700 transition-all rounded-md font-bold">
+            Education
+          </a>
+          <a href="#skills" className="block px-4 py-2 text-gray-900 dark:text-white hover:bg-opacity-70 hover:bg-blue-900 dark:hover:bg-blue-700 transition-all rounded-md font-bold">
+            Skills
+          </a>
+          <a href="#projects" className="block px-4 py-2 text-gray-900 dark:text-white hover:bg-opacity-70 hover:bg-blue-900 dark:hover:bg-blue-700 transition-all rounded-md font-bold">
+            Projects
+          </a>
+          <a href="#contact" className="block px-4 py-2 text-gray-900 dark:text-white hover:bg-opacity-70 hover:bg-blue-900 dark:hover:bg-blue-700 transition-all rounded-md font-bold">
+            Contact
+          </a>
+          {/* Theme Toggle */}
           <ThemeToggle />
         </div>
-        <div className="w-full h-full flex justify-center items-center overflow-hidden mt-5">
+
+        {/* Hamburger menu for mobile */}
+        <div className="absolute top-1 right-6 md:hidden">
+          <button onClick={() => setIsOpen(!isOpen)} className="focus:outline-none">
+            <GiHamburgerMenu className="w-8 h-8 text-gray-900 dark:text-white" />
+          </button>
+        </div>
+      </div>
+
+      {/* Mobile Menu */}
+      {isOpen && (
+        <div className="md:hidden bg-gradient-to-r from-gray-100 to-gray-300 dark:from-gray-800 dark:to-gray-900 rounded-lg shadow-lg mt-2 absolute inset-x-4 top-16 z-20">
+          <ul className="flex flex-col items-center space-y-2 py-4">
+            <li>
+              <a href="/" className="block px-4 py-2 text-gray-900 dark:text-white hover:bg-blue-900 dark:hover:bg-blue-700 transition-all rounded-md font-bold">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#education" className="block px-4 py-2 text-gray-900 dark:text-white hover:bg-blue-900 dark:hover:bg-blue-700 transition-all rounded-md font-bold">
+                Education
+              </a>
+            </li>
+            <li>
+              <a href="#skills" className="block px-4 py-2 text-gray-900 dark:text-white hover:bg-blue-900 dark:hover:bg-blue-700 transition-all rounded-md font-bold">
+                Skills
+              </a>
+            </li>
+            <li>
+              <a href="#projects" className="block px-4 py-2 text-gray-900 dark:text-white hover:bg-blue-900 dark:hover:bg-blue-700 transition-all rounded-md font-bold">
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href="#contact" className="block px-4 py-2 text-gray-900 dark:text-white hover:bg-blue-900 dark:hover:bg-blue-700 transition-all rounded-md font-bold">
+                Contact
+              </a>
+            </li>
+            <li>
+              {/* Theme Toggle */}
+              <ThemeToggle />
+            </li>
+          </ul>
+        </div>
+      )}
+      <div className="relative flex-grow container mx-auto px-4 py-8 flex flex-col items-center mt-20">
+        <div className="w-full h-full flex justify-center items-center overflow-hidden">
           <iframe
             src="/resume.pdf"
             title="Resume"
@@ -23,7 +92,7 @@ const ResumePage = () => {
           <a
             href="/resume.pdf"
             download
-            className="inline-block px-8 py-4 text-lg font-medium bg-gradient-to-r from-blue-500 to-purple-600 text-white rounded-full shadow-lg hover:from-blue-600 hover:to-purple-700 transition-all"
+            className="inline-block px-6 py-2 text-lg font-medium bg-gradient-to-r from-blue-500 to-purple-600 text-white rounded-full shadow-lg hover:from-blue-600 hover:to-purple-700 transition-all"
           >
             Download Resume
           </a>
